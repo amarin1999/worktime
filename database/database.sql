a512ed47de16106ae99af67211ba1df34847c67d
59122201005 <s59122201005@ssru.ac.th>
2020-04-21 14:05:33 +0700

    database

--- database/database.sql
+++ database/database.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `worktime` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `worktime`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
 -- Host: localhost    Database: worktime
@@ -55,9 +57,9 @@ CREATE TABLE `employee_has_sidework_history` (
   PRIMARY KEY (`employee_has_sidework_history_id`,`employee_id`),
   KEY `fk_employee_has_sidework_history_employee_idx` (`employee_id`),
   KEY `fk_employee_has_sidework_history_work_project1_idx` (`work_type`),
-  CONSTRAINT `fk_worktype` FOREIGN KEY (`work_type`) REFERENCES `worktype` (`id_work_type`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_employee_has_sidework_history_employee` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id_employee`)
-) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_employee_has_sidework_history_employee` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id_employee`),
+  CONSTRAINT `fk_worktype` FOREIGN KEY (`work_type`) REFERENCES `worktype` (`id_work_type`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=121 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +68,7 @@ CREATE TABLE `employee_has_sidework_history` (
 
 LOCK TABLES `employee_has_sidework_history` WRITE;
 /*!40000 ALTER TABLE `employee_has_sidework_history` DISABLE KEYS */;
-INSERT INTO `employee_has_sidework_history` VALUES (103,1,1),(105,1,1),(107,1,1),(108,1,1),(114,1,1),(115,1,1),(116,1,1),(117,1,1),(118,1,1),(119,1,1),(104,1,2),(106,1,2),(109,1,2),(110,1,2),(111,1,2),(112,1,2),(113,1,2);
+INSERT INTO `employee_has_sidework_history` VALUES (103,1,1),(105,1,1),(107,1,1),(108,1,1),(114,1,1),(115,1,1),(116,1,1),(117,1,1),(118,1,1),(119,1,1),(104,1,2),(106,1,2),(109,1,2),(110,1,2),(111,1,2),(112,1,2),(113,1,2),(120,1,2);
 /*!40000 ALTER TABLE `employee_has_sidework_history` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +90,7 @@ CREATE TABLE `ot_history` (
   PRIMARY KEY (`id_ot_history`),
   KEY `fk_employee_has_sidework_history_employee_idx` (`employee_has_sidework_history_id`),
   CONSTRAINT `fk side_employeehas0` FOREIGN KEY (`employee_has_sidework_history_id`) REFERENCES `employee_has_sidework_history` (`employee_has_sidework_history_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +99,7 @@ CREATE TABLE `ot_history` (
 
 LOCK TABLES `ot_history` WRITE;
 /*!40000 ALTER TABLE `ot_history` DISABLE KEYS */;
-INSERT INTO `ot_history` VALUES (73,104,'2563-03-24 15:26:52','2563-03-25 15:27:07','uf','2563-03-18 15:27:13','kp'),(74,104,'2563-03-24 15:26:52','2563-03-25 15:27:07','uf','2563-03-18 15:27:13','kp'),(75,106,'2563-03-19 13:51:16','2563-03-19 13:51:17',NULL,'2563-03-19 13:51:20','asd'),(81,112,'2563-03-23 14:50:52','2563-03-23 14:50:53','\n\n\n','2563-03-23 14:50:55','a2121'),(82,112,'2563-03-23 14:50:52','2563-03-23 14:50:53','\n\n\n','2563-03-23 14:50:55','a2121'),(83,113,'2563-03-23 14:52:22','2563-03-23 14:52:23',NULL,'2563-03-23 14:52:29','a321'),(84,113,'2563-03-23 14:52:22','2563-03-23 14:52:23',NULL,'2563-03-23 14:52:29','a321');
+INSERT INTO `ot_history` VALUES (73,104,'3106-03-24 15:26:00','3106-03-25 15:27:00','abc','2563-04-02 14:21:31','kp'),(74,104,'2563-03-24 15:26:52','2563-03-25 15:27:07','uf','2563-03-18 15:27:13','kp'),(75,106,'2563-03-19 13:51:16','2563-03-19 13:51:17',NULL,'2563-03-19 13:51:20','asd'),(85,120,'2563-04-02 12:25:07','2563-04-02 12:25:10',NULL,'2563-04-02 12:25:13','abc'),(86,120,'2563-04-02 12:25:07','2563-04-02 12:25:10',NULL,'2563-04-02 12:25:13','abc');
 /*!40000 ALTER TABLE `ot_history` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +122,7 @@ CREATE TABLE `sidework_history` (
   PRIMARY KEY (`id_sidework_history`),
   KEY `fk_employee_has_sidework_history_employee_idx` (`employee_has_sidework_history_id`),
   CONSTRAINT `fk side_employeehas` FOREIGN KEY (`employee_has_sidework_history_id`) REFERENCES `employee_has_sidework_history` (`employee_has_sidework_history_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -129,7 +131,7 @@ CREATE TABLE `sidework_history` (
 
 LOCK TABLES `sidework_history` WRITE;
 /*!40000 ALTER TABLE `sidework_history` DISABLE KEYS */;
-INSERT INTO `sidework_history` VALUES (53,118,'12:00:00','13:00:00',NULL,'2563-03-31 12:22:47',1,'2563-03-31'),(54,119,'12:00:00','18:00:00',NULL,'2563-03-31 12:58:47',0,'2563-03-31');
+INSERT INTO `sidework_history` VALUES (53,118,'10:00:00','15:00:00','a','2563-03-31 14:36:59',1,'2563-03-31');
 /*!40000 ALTER TABLE `sidework_history` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -198,4 +200,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-31 13:01:26
+-- Dump completed on 2020-04-21 14:04:49
