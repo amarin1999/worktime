435206a1b5285551a6971647114391062f53dcfb
59122201005 <s59122201005@ssru.ac.th>
2020-04-21 14:06:10 +0700

    Delete data.sql

--- data.sql
+++
@@ -1,4 +0,0 @@
-SELECT sh.start_time,sh.end_time,sh.work_comment 
-			 FROM worktime.sidework_history sh 
-			 JOIN worktime.employee_has_sidework_history esh 
-			JOIN worktime.employee e WHERE e.id_employee= 1
