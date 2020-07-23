4c60283f3dba430cead4f2aa3fd658dde3d92b52
mekpongsathon <Pongsathon.Rakthong@gmail.com>
2020-07-23 08:33:59 +0700

    edit year ot and sidework

---
+++ frontend/src/app/shared/pipe/format-year-ot.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FormatYearOtPipe } from './format-year-ot.pipe';
+
+describe('FormatYeraOtPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FormatYearOtPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
