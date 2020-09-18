e2c545bf9d609af76b598c24ff4108cd2f7a80ee
Yotsapat <patsuju0409@hotmail.com>
2020-09-18 13:57:33 +0700

    show emp

--- frontend/src/app/content/sidework/show-emp-by-day/show-emp-by-day.component.ts
+++ frontend/src/app/content/sidework/show-emp-by-day/show-emp-by-day.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, AfterViewInit, OnInit} from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { EmployeeByDay } from 'src/app/shared/interfaces/employee-by-day';
@@ -6,6 +6,7 @@ import { EmployeeByDayService } from 'src/app/shared/service/employee-by-day.ser
 import { MatDialogRef } from '@angular/material/dialog';
 import { SideWorkComponent } from '../sidework.component';
 import { Subscription } from 'rxjs';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -16,19 +17,50 @@ import { Subscription } from 'rxjs';
 export class ShowEmpByDayComponent implements OnInit {
   displayedColumns: string[] = ['employeeNo', 'firstname', 'lastname', 'workAnywhere'];
   employeeList: EmployeeByDay[];
-  selectWork: string = '1';
-  works: string[] = ['1', '2', '4', '3'];
-
+  formGroupEmp: FormGroup;
+  workAnyWhereSelect: any;
   dataSource = new MatTableDataSource<EmployeeByDay>();
 
 
   constructor(
     private empService: EmployeeByDayService,
     private dialogRef: MatDialogRef<SideWorkComponent>,
+    private buildForm: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    this.createFormEmp();
     this.qureyEmployeeByDay();
+  }
+
+  createFormEmp(): void {
+    this.formGroupEmp = this.buildForm.group({
+      workAnyWhere: [1],
+    })
+  }
+
+  workAnyWhereClick() {
+    const year = this.dialogRef.componentInstance.dataForm.date.getUTCFullYear();
+    const month = this.dialogRef.componentInstance.dataForm.date.getUTCMonth() + 1;
+    const day = this.dialogRef.componentInstance.dataForm.date.getUTCDate() + 1;
+
+    if (this.formGroupEmp.get('workAnyWhere').value == 1) {
+      this.workAnyWhereSelect = 1;
+      console.log('11111')
+    } else if (this.formGroupEmp.get('workAnyWhere').value == 2) {
+      this.workAnyWhereSelect = 2;
+      console.log('22222')
+    } else if (this.formGroupEmp.get('workAnyWhere').value == 3) {
+      this.workAnyWhereSelect = 3;
+    } else if (this.formGroupEmp.get('workAnyWhere').value == 4) {
+      this.workAnyWhereSelect = 4;
+    }
+
+    this.empService.getEmployeeByDay(year, month, day, this.workAnyWhereSelect).subscribe((list) => {
+      this.employeeList = list;
+      this.dataSource = new MatTableDataSource<EmployeeByDay>(this.employeeList);
+    }), error => {
+    }
   }
 
 
@@ -37,9 +69,7 @@ export class ShowEmpByDayComponent implements OnInit {
     const month = this.dialogRef.componentInstance.dataForm.date.getUTCMonth() + 1;
     const day = this.dialogRef.componentInstance.dataForm.date.getUTCDate() + 1;
 
-    const work = this.selectWork;
-
-    this.empService.getEmployeeByDay(year, month, day, work).subscribe((list) => {
+    this.empService.getEmployeeByDay(year, month, day, 1).subscribe((list) => {
       this.employeeList = list;
       this.dataSource = new MatTableDataSource<EmployeeByDay>(this.employeeList);
       // this.dataSource.paginator = this.paginator;
@@ -47,16 +77,6 @@ export class ShowEmpByDayComponent implements OnInit {
 
     }
   }
-
-  // clickSelect() {
-  //   const work = this.selectWork;
-  //   this.qureyEmployeeByDay(work)
-  // }
-
-  // ngOnDestroy() {
-  //   this.empService.unsubscribe()
-  // }
-
 }
 
 
